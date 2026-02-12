82d97b4739ea01b3a2fa581ef299f91e3b2aca84
alpesh <kamblealpha1@gmail.com>
2026-02-12 11:30:55 +0530

    12-02

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -852,8 +852,8 @@ export default function Profile() {
 
       {/* Buttons */}
       <div className="form-buttons">
-        <button type="submit" className="btn-save">
-          Save Changes
+        <button type="submit" className="btn-change">
+          Change
         </button>
 
         <button
